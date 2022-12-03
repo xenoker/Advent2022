a88ee74663b8182a211355035839e8442f6a0208
Xenoker <xenoker@users.noreply.github.com>
2022-12-03 14:16:07 -0500

    day 3

---
+++ 3.py
@@ -0,0 +1,15 @@
+from puzzleinput import getlines
+L = getlines(3)
+
+def pri(x):
+    return ord(x)>96 and ord(x)-96 or ord(x)-38
+
+def part1():
+    return sum(pri((set(l[:len(l)//2])&set(l[len(l)//2:])).pop())for l in L)
+
+def part2():
+    return sum(pri((set(L[i*3])&set(L[i*3+1])&set(L[i*3+2])).pop())for i in range(len(L)//3))
+
+if __name__ == '__main__':
+    assert part1()==7746
+    assert part2()==2604
