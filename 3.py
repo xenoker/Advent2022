343aa5c691cd7087bd35b7982e66e73e8b08aafb
Xenoker <xenoker@users.noreply.github.com>
2022-12-03 15:54:02 -0500

    alternate 3 part 2

--- 3.py
+++ 3.py
@@ -10,6 +10,10 @@ def part1():
 def part2():
     return sum(pri((set(L[i*3])&set(L[i*3+1])&set(L[i*3+2])).pop())for i in range(len(L)//3))
 
+#from functools import reduce
+#def part2(): #better, but longer and needs to import reduce
+#    return sum(pri((reduce(set.intersection,map(set,L[i:i+3]))).pop())for i in range(0,len(L),3))
+
 if __name__ == '__main__':
     assert part1()==7746
     assert part2()==2604
