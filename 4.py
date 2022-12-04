c27e933666e3db961cf4239e234f971b94dcdee3
Xenoker <xenoker@users.noreply.github.com>
2022-12-04 00:47:12 -0500

    day 4

---
+++ 4.py
@@ -0,0 +1,18 @@
+from puzzleinput import getlines
+
+def rset(x):
+    x1,x2=map(int,x.split('-'))
+    return set(range(x1,x2+1))
+
+L = [tuple(map(rset,l.split(',')))for l in getlines(4)]
+
+def part1():
+    return sum(a.issubset(b)or b.issubset(a)for a,b in L)
+
+def part2():
+    return sum(bool(a&b)for a,b in L)
+
+if __name__ == '__main__':
+    assert part1()==471
+    assert part2()==888
+
